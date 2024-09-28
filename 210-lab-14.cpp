e1cce16e86a6fc1f77579f5f3ec1e9583aa57367
Vevaan <vevaan.s.verma@gmail.com>
2024-09-27 19:12:44 -0700

    Add color testing to main method

--- 210-lab-14.cpp
+++ 210-lab-14.cpp
@@ -1,6 +1,7 @@
 // COMSC-210 | Lab 14 | Vevaan Verma
 using namespace std;
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 // class Color represents a color with red, green, and blue components
@@ -12,22 +13,26 @@ private:
 	int blue;
 
 public:
-	// getters and setters for the red component
+	// getter and setter for the red component
 	void setRed(int r) { red = r; }
 	int getRed() const { return red; }
 
-	// getters and setters for the green component
+	// getter and setter for the green component
 	void setGreen(int g) { green = g; }
 	int getGreen() const { return green; }
 
-	// getters and setters for the blue component
+	// getter and setter for the blue component
 	void setBlue(int b) { blue = b; }
 	int getBlue() const { return blue; }
 
-	// print() prints the color in the format "Color: [VALUE]R, [VALUE]G, [VALUE]B"
+	// print() prints the color in the format "Color: [RED VALUE], [GREEN VALUE], [BLUE VALUE]"
 	// arguments: none
 	// returns: void
-	void print() const { cout << "Color: " << red << "R, " << green << "G, " << blue << "B" << endl; }
+	void print() const {
+
+		cout << "RGB Code: " << setw(3) << red << ", " << setw(3) << green << ", " << setw(3) << blue << endl;
+
+	}
 
 };
 
@@ -36,5 +41,35 @@ public:
 // returns: int - the exit code of the program
 int main() {
 
+	Color red;
+	red.setRed(255);
+	red.setGreen(0);
+	red.setBlue(0);
+
+	Color green;
+	green.setRed(0);
+	green.setGreen(255);
+	green.setBlue(0);
+
+	Color blue;
+	blue.setRed(0);
+	blue.setGreen(0);
+	blue.setBlue(255);
+
+	Color orange;
+	orange.setRed(255);
+	orange.setGreen(165);
+	orange.setBlue(0);
+
+	Color purple;
+	purple.setRed(128);
+	purple.setGreen(0);
+	purple.setBlue(128);
+
+	red.print();
+	green.print();
+	blue.print();
+	orange.print();
+	purple.print();
 
 }
